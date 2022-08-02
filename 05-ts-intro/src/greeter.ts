f0c0d5a1d2e1f0ed87ed6e659c92e9a44eb6ca1b
Boris Gerov <boris.gerov@Boriss-Mac-mini-2.local>
2022-08-02 17:36:34 +0300

    05-ts-intro Typescript

--- 05-ts-intro/src/greeter.ts
+++ 05-ts-intro/src/greeter.ts
@@ -1,9 +1,65 @@
-function greeter(name: string): string{
-    return `Hello ${name}! from Typescript`;
+function greeter(name: string, date: Date = new Date()): string {
+  return `Hello ${name}! from Typescript on ${date.toDateString()}`;
 }
 
-// document.getElementById('results')!.innerHTML = greeter('Boris'); // we have risk if the element is not in the HTML file, so we use this ->
+// document.getElementById("results")!.innerHTML = greeter("Boris"); // we have risk if the element is not in the HTML file, so we use this ->
+
 const elem = document.getElementById('results');
-if(elem != null) {
+if(elem !== null) {
     elem.innerHTML = greeter('Boris');
 }
+
+
+function printId(id: number | string) {
+    if(typeof id === "string"){
+        console.log("Your ID is: " + id.toUpperCase());
+    } else {
+        console.log("Your ID is: " + ++id);
+    }
+    console.log("Your ID is: " + id);
+}
+
+function welcomePeople(x: string[] | string) {
+    if (Array.isArray(x)) {
+      // Here: 'x' is 'string[]'
+      console.log("Hello, " + x.join(" and "));
+    } else {
+      // Here: 'x' is 'string'
+      console.log("Welcome lone traveler " + x);
+    }
+}
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+      console.log(x.toUTCString());
+    } else {
+      console.log(x.toUpperCase());
+    }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+ 
+function move(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    return animal.swim();
+  }
+ 
+  return animal.fly();
+}
+
+let x = Math.random() < 0.5 ? 10 : "hello world!";
+x = 1;
+console.log(x);
+x = "string";
+console.log(x);
+
+
+// printId(40);
+// printId("50")
+
+// welcomePeople("me");
+// welcomePeople(["me","you"]);
+
+// logValue("Boris");
+// logValue(Date());
