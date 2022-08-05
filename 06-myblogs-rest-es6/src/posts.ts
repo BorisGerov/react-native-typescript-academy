745aac1de6fe074debd9e43534e68ade5e584ce5
Boris Gerov <boris.gerov@Boriss-Mac-mini-2.local>
2022-08-05 12:27:03 +0300

    Added more Typescript

--- 06-myblogs-rest-es6/src/posts.ts
+++ 06-myblogs-rest-es6/src/posts.ts
@@ -1,3 +1,4 @@
+import { NumberIdGenerator, Repository, RepositoryInMemoryImpl } from "./repository.js";
 import { IdType } from "./shared-types.js";
 
 export class PostCreateDto {
@@ -22,3 +23,39 @@ export class Post extends PostCreateDto{
         super(title, content, tags, imageUrl, authorId);
     }
 }
+
+export interface PostRepository extends Repository<IdType, Post> {
+    findByTags(searchTags: string[]): Post[];
+    findByTitlePart(titlePart: string): Post[];
+    findByAuthorId(authorId: IdType): Post[];
+}
+
+export class PostRepositoryImpl extends RepositoryInMemoryImpl<IdType, Post> implements PostRepository {
+    findByTags(searchTags: string[]): Post[] {
+        return this.findAll().filter(post => post.tags.some(tag => searchTags.includes(tag)));
+    }
+    findByTitlePart(titlePart: string): Post[] {
+        return this.findAll().filter(post => post.title.includes(titlePart));
+    }
+    findByAuthorId(authorId: number): Post[] {
+        return this.findAll().filter(post => post.authorId === authorId);
+    }
+}
+
+const SAMPLE_POST = [
+    new Post(0, "New in Typescript","Typescript becomes stricter ...", ['typescript','novelties'],"https://dz2cdn1.dzone.com/storage/temp/8746383-json-server.png",1),
+    new Post(0, "New in Typescript","EcmaScript becomes stricter ...", ['es','js'],"https://res.cloudinary.com/practicaldev/image/fetch/s--Iz3e8EkG--/c_imagga_scale,f_auto,fl_progressive,h_1080,q_auto,w_1080/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/azhvn8vuqrofsidykaas.jpg",1),
+    new Post(0, "New in Typescript","AsyncComposition becomes stricter ...", ['async','ajax'],"http://doctorakil.com/wp-content/uploads/2018/01/coffee-940x600.jpg",1)
+]
+
+function testPostrepository() {
+    const postRepo: PostRepository = new PostRepositoryImpl(new NumberIdGenerator());
+    SAMPLE_POST.forEach(post => postRepo.create(post));
+    postRepo.findAll().forEach(post => console.log(post));
+    console.log('Find by Tags: ');
+    postRepo.findByTags(['es']).forEach(post => console.log(post));
+    console.log('Find by Title: ');
+    postRepo.findByTitlePart('New').forEach(post => console.log(post));
+}
+
+testPostrepository();
