00c89e6b46029fdaf9cbfd3801ba9f180f275490
Boris Gerov <boris.gerov@Boriss-Mac-mini-2.local>
2022-08-30 18:01:35 +0300

    15-react-native-apis-comps Progress Bar and Flatlist updated

--- 15-react-native-apis-comps/App.tsx
+++ 15-react-native-apis-comps/App.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import AppAnimation01 from "./AppAnimation01";
 import AppAnimation02 from "./AppAnimation02";
 import AppEasing from "./AppEasing";
+import ProgressBar from "./AppProgressBar";
+import AppProgressBar from "./AppProgressBar";
 import Stagger from "./Stagger";
 
 export default () =>
-(<Stagger />)
+(<ProgressBar min={0} max={100} onFinish={() => console.log("Finished")}/>)
