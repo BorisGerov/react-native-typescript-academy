7ab30ac19d5933629afca253b2f7d977343ca9ce
Boris Gerov <boris.gerov@Boriss-Mac-mini-2.local>
2022-09-05 09:11:36 +0300

    15-react-native-apis-comps AppToaster updated

--- 15-react-native-apis-comps/App.tsx
+++ 15-react-native-apis-comps/App.tsx
@@ -11,25 +11,54 @@ interface AppState {
     current : number;
 }
 
-export default class App extends Component<{}, AppState> {
+// export default class App extends Component<{}, AppState> {
+//     state: Readonly<AppState> = {
+//         current: 0,
+//     };
+
+//     handlePress = () => {
+//         if(this.state.current < 100){
+//         this.setState({current: this.state.current + 25})
+//         }
+//     }
+
+//     render(){
+//         return(
+//      <View> 
+//         <Pressable onPress={this.handlePress}>
+//             <Text>Click here !</Text> 
+//         </Pressable>
+//     <ProgressBar min={0} max={100} current={this.state.current}/>
+//     </View>
+//         )
+//     }
+// }
+
+
+import Toaster from './AppToaster'
+
+interface AppState {
+    visible: boolean;
+}
+export default class App extends Component {
     state: Readonly<AppState> = {
+        visible: false,
         current: 0,
-    };
-
-    handlePress = () => {
-        if(this.state.current < 100){
-        this.setState({current: this.state.current + 25})
-        }
     }
-
-    render(){
-        return(
-     <View> 
-        <Pressable onPress={this.handlePress}>
-            <Text>Click here !</Text> 
+    showToaster = () => {
+        this.setState({visible: true})
+    }
+    hideToaster = () => {
+        this.setState({visible: false})
+    }
+  render() {
+    return (
+        <View>
+        <Pressable onPress={this.showToaster}>
+            <Text>Click here!</Text>
         </Pressable>
-    <ProgressBar min={0} max={100} current={this.state.current}/>
-    </View>
-        )
-    }
+        <Toaster delay={4000} message={'You got a new message!'} duration={500} positionX={320} positionY={100} visible={this.state.visible}/>
+        </View>
+    )
+  }
 }
